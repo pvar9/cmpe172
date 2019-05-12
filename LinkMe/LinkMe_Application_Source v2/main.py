3d67b34fcab499369c495a98f97f107f147e7ff6
Navina Mathew <mathewnavina@gmail.com>
2019-05-11 23:19:45 -0700

    adding pyopenssl https feature

--- LinkMe/LinkMe_Application_Source v2/main.py
+++ LinkMe/LinkMe_Application_Source v2/main.py
@@ -121,4 +121,4 @@ def contact_send(send):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=9000, debug=True)
+    app.run(host="0.0.0.0", port=9000, debug=True, ssl_context='adhoc')
